269a62ded43d8902f0f2871aad8b91729a6f9aa2
xXSirGamesXx <xXSirGamesXx@gmail.com>
2017-07-24 15:24:16 -0400

    Probably desnt work yet bc i dont test

--- src/xXSirGamesXx/CommandCharger/Main.php
+++ src/xXSirGamesXx/CommandCharger/Main.php
@@ -12,7 +12,7 @@ use pocketmine\event\player\PlayerCommandPreprocessEvent;
 use onebone\economyapi\EconomyAPI;
 
 class Main extends PluginBase implements Listener {
-	const PREFIX_SUCCESS = C::WHITE . "[" . C::RED . "CommandCharger" . C::WHITE . "] " . C::GREEN; 
+	const PREFIX = C::WHITE . "[" . C::RED . "CommandCharger" . C::WHITE . "] " . C::GREEN; 
 		
 		public function onLoad() {
 		$this->getLogger()->info("Loading CommandCharger");
@@ -28,13 +28,30 @@ class Main extends PluginBase implements Listener {
 		$this->saveDefaultCnfig();
 	}
 	
-	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
 		if($cmd->getName() == "commandcharge") { 
-			$sender->sendMessage(self::PREFIX_SUCCESS . "CommandCharger by xXSirGamesXx.");
+			$sender->sendMessage(self::PREFIX . "CommandCharger by xXSirGamesXx.");
 		}
 	}
 	public function onPCommand(PlayerCommandPreprocessEvent $e){
 		$player = $e->getPlayer();
 		$msg = $e->getMessage();
+		$money = $this->api->myMoney($player);
+		$cfg = $this->getConfig()->getAll();
+		foreach($cfg["Commands"] as $c){
+			if(!$player->hasPermission("cc.charge.avoid")){
+				if(strtolower($msg) == $c["CMD"] and $this->api->myMoney($player) >= $c["Price"]){
+					$player->sendMessage(self::PREFIX . "You have been charged " . C::YELLOW . $c["Price"] ."$" . C::GREEN . "For using " . $c["CMD"]);
+					$this->api->reduceMoney($player, $c["Price"]);
+				}else{
+					$player->sendMessage(self::PREFIX . C::RED . "You do not have enough money for " . C::YELLOW . $c["CMD"]);
+					$e->setCancelled(true);
+				}
+			}else{
+				$player->sendMessage(self::PREFIX . "You were not charged for using " . C::AQUA . $c["CMD"]);
+			}
+		}
+				
 	}
 }
+
